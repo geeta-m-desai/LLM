a705a088b84484c9cbb31d8f282957fcb69228c4
GeetaDesai <geeta.m.desai@gmail.com>
2023-09-08 19:16:40 +0530

    AutoGpt example

--- langchain_new/langchain_doc_summarizer.py
+++ langchain_new/langchain_doc_summarizer.py
@@ -30,12 +30,13 @@ if st.button("Summarize"):
             # Create embeddings for the pages and insert into Chroma database
             embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
             vectordb = Chroma.from_documents(pages, embeddings)
-
+            query="Write a summary of the document. Provide list of research papers discussed in the document in a bullet list."
+            vectordb.max_marginal_relevance_search(query, k=2)
             # Initialize the OpenAI module, load and run the summarize chain
             llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
             chain = load_summarize_chain(llm, chain_type="stuff")
             search = vectordb.similarity_search(" k=1 ")
-            summary = chain.run(input_documents=search, question="Write a summary of the document. Provide list of research papers discussed in the document in a bullet list.")
+            summary = chain.run(input_documents=search)
 
             st.write(summary)
         except Exception as e:
